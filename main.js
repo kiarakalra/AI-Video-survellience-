9890264adab6989a016029fb8b73ea0162c8edd9
kiarakalra <71638118+kiarakalra@users.noreply.github.com>
2020-11-09 15:57:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-video="";
+objects=[];
 status="";
 
 function preload() 
@@ -14,10 +14,35 @@ function setup()
     canvas.center();
 }
 
+function gotResult(error,results)
+{ 
+ if (error)
+ {
+    console.log(error);
+}
+    console.log(results);
+    objects = results;
+}
 
 function draw() 
 {
     image(video,0,0,480,380);
+     if (status !="") 
+     {
+         objectDetector.detect(video, gotResult);
+         for (i = 0; i < objects.length; i++)
+         {
+             document.getElementById("status").innerHTML = "status : objects detected";
+             document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ object.length;
+            
+             fill("#d8b7eb");
+             percent = floor(objects[i].confidence * 100);
+             text(objects[i].label + " " + percent + "%", objects[i].x + 15, object[i].y + 15);
+            noFill();
+            stroke("#d8b7eb");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+     }
 }
 
 
